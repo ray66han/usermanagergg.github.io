ba3ec9487dd37b9fbc0d0cad7efc92c1d96137cc
rayan <rayhan66chowdhury@gmail.com>
2022-12-21 21:48:39 +0000

    all folders added

---
+++ admin/index.php
@@ -0,0 +1,14 @@
+<!-- header link -->
+<?php
+    include 'header.php';
+?>
+<!-- body items goes here -->
+<?php
+    include 'sign-in.php';
+?>
+
+
+<!-- footer link -->
+<?php
+    include 'footer.php';
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    // header
+    include 'guest/header.php';
+
+    // Main snippets goes here
+    include 'guest/log-in.php';
+    // footer
+    include 'guest/footer.php;'
+
+?>
